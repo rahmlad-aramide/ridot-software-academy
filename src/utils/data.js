d639cb09775795ec8e5b59299e852612ee6dcfcb
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2023-11-15 15:31:37 +0100

    update: added 1k for testing

--- src/utils/data.js
+++ src/utils/data.js
@@ -60,65 +60,65 @@ export const pricingData = [
         },
       ],
     },
-    // {
-    //   title: "Backend Development",
-    //   subtitle: "Backend engineers handle server-side technologies, manage databases, and build robust web applications.",
-    //   duration: "3 months",
-    //   times: "2 times",
-    //   installments: "70% down payment",
-    //   price: 25000,
-    //   discount: '35,000',
-    //   priceBg: "#020181",
-    //   buttonVal: "Enroll Now",
-    //   percent: "27%",
-    //   link: "/",
-    //   desc: [
-    //     {
-    //       icon: check,
-    //       text: "Introduction to frontend engineering career path",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Overview of web Development- Learn how the web works and the different technologies that power the web",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Fundamentals of HTML- learn how to structure web pages using HTML",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Fundamentals of CSS- learn how to apply styles to Html documents using CSS",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Developing Websites- Get acquainted with text editors and chrome DevTools to develop websites locally",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Deploying websites- Learn how to deploy websites using GitHub pages and the commandline",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Fundamentals of Web Design- Learn how to design websites using Figma",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Making a website responsive- Learn how to utilize responsive web design practices using CSS grid, flexbox and media queries ",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Making a website accessible- Learn how to make websites accessible to everyone",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Free two months mentorship- Get paired with a mentor for two months to guide you on your journey",
-    //     },
-    //     {
-    //       icon: check,
-    //       text: "Certificate of completion- Get a certificate of completion after the program",
-    //     },
-    //   ],
-    // },
+    {
+      title: "Testing Dev",
+      subtitle: "This is for testing the codeBackend engineers handle server-side technologies, manage databases, and build robust web applications.",
+      duration: "3 months",
+      times: "2 times",
+      installments: "70% down payment",
+      price: 1000,
+      discount: '35,000',
+      priceBg: "#020181",
+      buttonVal: "Enroll Now",
+      percent: "97%",
+      link: "/",
+      desc: [
+        {
+          icon: check,
+          text: "Introduction to frontend engineering career path",
+        },
+        {
+          icon: check,
+          text: "Overview of web Development- Learn how the web works and the different technologies that power the web",
+        },
+        {
+          icon: check,
+          text: "Fundamentals of HTML- learn how to structure web pages using HTML",
+        },
+        {
+          icon: check,
+          text: "Fundamentals of CSS- learn how to apply styles to Html documents using CSS",
+        },
+        {
+          icon: check,
+          text: "Developing Websites- Get acquainted with text editors and chrome DevTools to develop websites locally",
+        },
+        {
+          icon: check,
+          text: "Deploying websites- Learn how to deploy websites using GitHub pages and the commandline",
+        },
+        {
+          icon: check,
+          text: "Fundamentals of Web Design- Learn how to design websites using Figma",
+        },
+        {
+          icon: check,
+          text: "Making a website responsive- Learn how to utilize responsive web design practices using CSS grid, flexbox and media queries ",
+        },
+        {
+          icon: check,
+          text: "Making a website accessible- Learn how to make websites accessible to everyone",
+        },
+        {
+          icon: check,
+          text: "Free two months mentorship- Get paired with a mentor for two months to guide you on your journey",
+        },
+        {
+          icon: check,
+          text: "Certificate of completion- Get a certificate of completion after the program",
+        },
+      ],
+    },
     {
       title: "Product Design",
       subtitle: "Product design deals with the design of user interface and experience of a website for a better performance.",
