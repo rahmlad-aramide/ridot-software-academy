e26bba384d4f7b931cab5beb2b258e86701fbb2b
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2023-11-18 12:28:10 +0100

    update: updated blog page

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,25 +1,50 @@
-import { Link } from "react-router-dom";
+import { useState } from 'react'
 import { Layout } from "../../components";
 
 const Blog = () => {
+  const [currentCategory, setCurrentCategory] = useState('all');
+  const [selectedValue, setSelectedValue] = useState('all');
+  
+  const handleSelect = e => {
+  setSelectedValue(e.target.value)
+  }
+  
+  const handleCategory = (cat) => {
+  setCurrentCategory(cat)
+  }
+  
   return (
     <Layout>
-      <div className="flex h-screen items-center justify-center">
-        <div className="max-w-lg text-center">
-          <h1 className="mb-8 text-9xl font-extrabold text-primary">🛠️ </h1>
-          <h2 className="mb-4 text-gray-600 px-4 text-2xl">
-            Under Construction
-          </h2>
-          <p className="mb-8 text-gray-600">
-            This page is currently under construction, check back later.
-          </p>
-          <Link to="/">
-            <button className="rounded-lg bg-primary py-2 px-6 font-bold text-white hover:scale-90 active:scale-100 transition duration-300">
-              Go back Home
-            </button>
-          </Link>
+        <div className='w-full pt-5'>
+            <div className='block my-2 bg-secondary w-20 h-1 mx-auto sm:mx-0'></div>
+          <div className='flex flex-col sm:flex-row justify-between pb-1 md:pb-0'>
+            <div className='text-dark font-semibold text-xl text-center md:text-left uppercase text-primary'>Blog Posts</div>
+            <div className="hidden md:flex gap-[10px]">
+              <button onClick={()=>handleCategory('all')} className={currentCategory==='all' ? "px-[10px] py-[2px] bg-[#CED4DC] rounded text-[13px]": "px-[10px] py-[2px] text-[13px]"}>All Posts</button>
+              <button onClick={()=>handleCategory('web')} className={currentCategory==='web' ? "px-[10px] py-[2px] bg-[#CED4DC] rounded text-[13px]": "px-[10px] py-[2px] text-[13px]"}>Web Design</button>
+              <button onClick={()=>handleCategory('product')} className={currentCategory==='product' ? "px-[10px] py-[2px] bg-[#CED4DC] rounded text-[13px]": "px-[10px] py-[2px] text-[13px]"}>Product Design</button>
+              <button onClick={()=>handleCategory('marketing')} className={currentCategory==='marketing' ? "px-[10px] py-[2px] bg-[#CED4DC] rounded text-[13px]": "px-[10px] py-[2px] text-[13px]"}>Digital Marketing</button>
+            </div>
+            <div className='flex justify-center items-center md:hidden text-sm font-medium mt-2 md:my-auto'>
+              <label htmlFor='filter'>
+                Filter by: 
+              </label>
+              <select id='filter' value={selectedValue} onChange={handleSelect} className='ml-[10px] bg-[#CED4DC] rounded p-2'>
+                <option value="all">All Posts</option>
+                <option value="web">Web Design</option>
+                <option value="product">Product Design</option>
+                <option value="marketing">Digital Marketing</option>
+              </select>
+            </div>
+          </div>
+          <div className='my-5 bg-primary w-full h-[1px]'></div>
+          <div className="h-[calc(100vh_-_250px)] flex justify-center items-center">
+            <div className="flex justify-center mx-2 text-gray-500">No blog post yet</div>
+          </div>
         </div>
-      </div>
+        <div className='hidden justify-center'>
+          <button disabled={true} className='bg-primary text-light rounded-lg px-3 py-[14px] my-10 font-medium flex shrink-0 transition duration-200 active:scale-100 hover:scale-90'>Load More</button>
+        </div>
     </Layout>
   );
 };
