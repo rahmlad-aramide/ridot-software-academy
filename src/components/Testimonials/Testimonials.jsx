65d9419e50c26e8325bb40c617544d83039c0bea
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2024-01-14 18:38:46 +0100

    update: added team and testimonial

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -42,6 +42,8 @@ const Testimonials = () => {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000, 
         centerMode: true,
         focusOnSelect: true,
         beforeChange: (current, next) => setCurrentSlide(next),
