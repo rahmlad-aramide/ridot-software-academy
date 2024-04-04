5d32ac100989eeca01fe13f2182e13f5e3eacc0f
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2024-04-04 22:05:18 +0100

    update: updated the pricing

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -2,23 +2,12 @@ import { useState } from "react";
 import { Layout } from "../../components";
 import { pricingData } from "../../utils/data";
 import underline from "../../assets/images/underline.svg";
-import { useNavigate } from "react-router-dom";
 import { PaystackCheckout } from "../../utils";
 
 const Pricing = () => {
-  const navigateTo = useNavigate();
-  const [selectedVal, setSelectedVal] = useState("Pick a plan");
   const [selectedPrice, setSelectedPrice] = useState(null);
   const [selectedPrices, setSelectedPrices] = useState([]);
   const [openModal, setOpenModal] = useState(false);
-  const handleChange = (e) => {
-    setSelectedVal(e.target.value);
-  };
-  const handleNavigate = (link) => {
-    if (selectedVal !== "Pick a plan") {
-      navigateTo(link);
-    }
-  };
 
   return (
     <Layout>
@@ -51,95 +40,83 @@ const Pricing = () => {
                       <p className="max-w-[60ch] mt-4">{data.subtitle}</p>
                       <div className="grid gap-2 md:gap-4 grid-cols-1 sm:grid-cols-2 mt-4">
                         <div>
-                          <span className="font-medium">Course Duration:</span> {data.duration}
+                          <span className="font-medium">Course Duration:</span>{" "}
+                          {data.duration}
                         </div>
                         <div>
-                          <span className="font-medium">Class Type:</span> Virtual training
+                          <span className="font-medium">Level:</span> Beginner -
+                          Intermediate
                         </div>
                         <div>
-                          <span className="font-medium">Installment:</span> 70% on admission, 30% after one month
+                          <span className="font-medium">Class Type:</span>{" "}
+                          Virtual training
                         </div>
                         <div>
-                          <span className="font-medium">Class Times:</span> {data.times}
+                          <span className="font-medium">Installment:</span> 30%
+                          and 70%
+                        </div>
+                        <div>
+                          <span className="font-medium">Class Times:</span>{" "}
+                          {data.times}
                         </div>
                       </div>
                     </div>
                     <div className="h-[1px] w-full bg-[#2d2d2d80] opacity-50 mt-10"></div>
-                    {data.title === "Custom" ? (
-                      <div className="h-[120px]">
-                        <select
-                          onChange={handleChange}
-                          className="border border-[#A25DDC] outline-none mt-6 h-[70px] rounded-lg w-full text-lg font-bold"
-                        >
-                          <option defaultValue={"Pick a plan"}>
-                            Pick a plan
-                          </option>
-                          <option value="monthly">Monthly</option>
-                          <option value="quarterly">Quarterly</option>
-                          <option value="annually">Annually</option>
-                          <option value="biannually">Bi-anually</option>
-                        </select>
-                      </div>
-                    ) : (
-                      <div className="flex items-center my-8 mt-10">
-                        <span
-                          style={{ color: data.priceBg }}
-                          className={`text-[42px] md:text-[62px] font-semibold leading-6 mr-[0.625rem]`}
-                        >
-                          <span>₦</span>
-                          {data.price.toLocaleString()}
-                        </span>{" "}
+                    <div className="flex items-center my-8 mt-10">
+                      <span
+                        style={{ color: data.priceBg }}
+                        className={`text-[42px] md:text-[62px] font-semibold leading-6 mr-[0.625rem]`}
+                      >
+                        <span>₦</span>
+                        {data.price.toLocaleString()}
+                      </span>{" "}
+                      {data.discount ? (
                         <div className="flex flex-col">
-                          <span>₦<s>{data.discount}</s></span>
+                          <span>
+                            ₦<s>{data.discount}</s>
+                          </span>
                           <span>{data.percent} Off</span>
                         </div>
-                      </div>
-                    )}
+                      ) : null}
+                    </div>
                     <div className="h-[1px] w-full bg-[#2d2d2d80] opacity-50 mb-10"></div>
                     {data.desc.map((desc, index) => (
                       <div key={index} className="flex items-center mb-4">
-                        {desc.icon &&
-                          <img src={desc.icon} alt={desc.text} />
-                        }
+                        {desc.icon && <img src={desc.icon} alt={desc.text} />}
                         <span className="ml-2">{desc.text}</span>
                       </div>
                     ))}
                   </div>
-                  {data.title === "Custom" ? (
-                    <div className="mb-2">
-                      <button
-                        onClick={() => handleNavigate(data.link)}
-                        disabled={selectedVal === "Pick a plan"}
-                        className="bg-primary hover:scale-95 disabled:bg-primary/40 transition duration-200 rounded-full text-white w-full py-2"
-                      >
-                        {data.buttonVal}
-                      </button>
-                    </div>
-                  ) : (
-                    <div className="mt-4 mb-2">
-                        <button onClick={()=>
-                          {
-                            setSelectedPrice(data.price)
-                            setSelectedPrices(data.prices)
-                            setOpenModal(true)
-                          }} 
-                          style={{backgroundColor: data.priceBg}} className="bg-primary hover:scale-95 transition duration-200 rounded-lg text-white w-full py-2">
-                          {data.buttonVal}
-                        </button>
-                    </div>
-                  )}
+
+                  <div className="mt-4 mb-2">
+                    <button
+                      onClick={() => {
+                        setSelectedPrice(data.price);
+                        setSelectedPrices(data.prices);
+                        setOpenModal(true);
+                      }}
+                      style={{ backgroundColor: data.priceBg }}
+                      className="bg-primary hover:scale-95 transition duration-200 rounded-lg text-white w-full py-2"
+                    >
+                      {data.buttonVal}
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
           </div>
         </div>
       </div>
-      {
-        (openModal && selectedPrice !== null) &&
+      {openModal && selectedPrice !== null && (
         <div className="h-screen w-full fixed z-50 top-0 left-0 bg-black/30 backdrop-blur-sm flex justify-center">
-          <PaystackCheckout selectedPrice={selectedPrice} selectedPrices={selectedPrices} setSelectedPrice={setSelectedPrice} setOpenModal={setOpenModal} />
+          <PaystackCheckout
+            selectedPrice={selectedPrice}
+            selectedPrices={selectedPrices}
+            setSelectedPrice={setSelectedPrice}
+            setOpenModal={setOpenModal}
+          />
         </div>
-      }
+      )}
     </Layout>
   );
 };
