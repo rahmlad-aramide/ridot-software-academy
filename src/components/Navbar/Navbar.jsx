008574f91ecabe7853bf35a74b9127e9c5a8e540
Abdrahman Oladimeji <abdrahmanoladimeji02@gmail.com>
2025-06-17 14:49:49 +0100

    feat: add digiskills for women ✨✅

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
-import { NavLink } from 'react-router-dom';
-import ridotLogo from './../../assets/images/logo.png';
-import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { NavLink } from "react-router-dom";
+import ridotLogo from "./../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import { useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Navbar = () => {
@@ -13,50 +13,105 @@ const Navbar = () => {
     const togglerClassList = togglerRef.current.classList;
 
     if (navClassList.contains("-translate-y-[calc(100%_+_200px)]")) {
-      navClassList.remove("-translate-y-[calc(100%_+_200px)]")
-      togglerClassList.add("crossed-bars")
+      navClassList.remove("-translate-y-[calc(100%_+_200px)]");
+      togglerClassList.add("crossed-bars");
     } else {
-      navClassList.add("-translate-y-[calc(100%_+_200px)]")
-      togglerClassList.remove("crossed-bars")
+      navClassList.add("-translate-y-[calc(100%_+_200px)]");
+      togglerClassList.remove("crossed-bars");
     }
-  }
+  };
 
   return (
     <>
       <header className="w-full fixed top-0 z-50">
-        <div className={`bg-white/30 backdrop-blur-lg shadow h-16 md:h-20 flex justify-center w-full z-50 border-b border-[rgba(0,0,0,0.20)] fixed md:static top-0'}`}>
-          <div className='flex justify-between items-center w-[90%] max-w-[1200px] mx-auto'>
+        <div
+          className={`bg-white/30 backdrop-blur-lg shadow h-16 md:h-20 flex justify-center w-full z-50 border-b border-[rgba(0,0,0,0.20)] fixed md:static top-0'}`}
+        >
+          <div className="flex justify-between items-center w-[90%] max-w-[1200px] mx-auto">
             <div className="w-fit">
               <Link to="/">
-                <img src={ridotLogo} alt="Ridot software academy" className="max-w-[140px]" />
+                <img
+                  src={ridotLogo}
+                  alt="Ridot software academy"
+                  className="max-w-[140px]"
+                />
               </Link>
             </div>
-            <button ref={togglerRef} onClick={handleOpenNav} className='fixed md:hidden top-6 right-8 sm:right-8 z-50 transition duration-200 hover:scale-125 active:scale-100'>
+            <button
+              ref={togglerRef}
+              onClick={handleOpenNav}
+              className="fixed md:hidden top-6 right-8 sm:right-8 z-50 transition duration-200 hover:scale-125 active:scale-100"
+            >
               <div>
-                <div className='bar w-6 h-1 mb-1 bg-black/80'></div>
-                <div className='bar w-6 h-1 mb-1 bg-black/80'></div>
-                <div className='bar w-6 h-1 mb-1 bg-black/80'></div>
+                <div className="bar w-6 h-1 mb-1 bg-black/80"></div>
+                <div className="bar w-6 h-1 mb-1 bg-black/80"></div>
+                <div className="bar w-6 h-1 mb-1 bg-black/80"></div>
               </div>
             </button>
-            <nav ref={navRef} className="bg-white -translate-y-[calc(100%_+_200px)] md:translate-y-0 fixed md:relative bg-light shadow-xl md:shadow-none rounded-lg border md:border-0 md:bg-transparent top-16 md:top-0 right-[180px] md:right-0 h-fit w-fit px-6 sm:px-8 py-4 md:px-0 translate-x-full md:translate-x-0 transition duration-200">
-              <ul className='flex items-center md:justify-center flex-col md:flex-row md:mt-0 w-full'>
-                <li className='mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2'><NavLink className={({ isActive }) => isActive ? "underline underline-offset-8" : ""} to="/" onClick={handleOpenNav}>
-                  Home
-                </NavLink>
+            <nav
+              ref={navRef}
+              className="bg-white -translate-y-[calc(100%_+_200px)] md:translate-y-0 fixed md:relative bg-light shadow-xl md:shadow-none rounded-lg border md:border-0 md:bg-transparent top-16 md:top-0 right-[180px] md:right-0 h-fit w-fit min-w-[165px] md:w-fit px-6 sm:px-8 py-4 md:px-0 translate-x-full md:translate-x-0 transition duration-200"
+            >
+              <ul className="flex items-center md:justify-center flex-col md:flex-row md:mt-0 w-full">
+                <li className="mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "underline underline-offset-8" : ""
+                    }
+                    to="/"
+                    onClick={handleOpenNav}
+                  >
+                    Home
+                  </NavLink>
                 </li>
-                <li className='mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2'><NavLink className={({ isActive }) => isActive ? "underline underline-offset-8" : ""} to="/about" onClick={handleOpenNav}>
-                  About us
-                </NavLink>
+                <li className="mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "underline underline-offset-8" : ""
+                    }
+                    to="/about"
+                    onClick={handleOpenNav}
+                  >
+                    About us
+                  </NavLink>
                 </li>
-                <li className='mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2'><NavLink className={({ isActive }) => isActive ? "underline underline-offset-8" : ""} to="/contact" onClick={handleOpenNav}>
-                  Contact us
-                </NavLink>
+                <li className="mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "underline underline-offset-8" : ""
+                    }
+                    to="/digiskills-for-women"
+                    onClick={handleOpenNav}
+                  >
+                    Digital Skills
+                  </NavLink>
                 </li>
-                <li className='mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2'><NavLink className={({ isActive }) => isActive ? "underline underline-offset-8" : ""} to="/blog" onClick={handleOpenNav}>
-                  Our Blog
-                </NavLink>
+                <li className="mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "underline underline-offset-8" : ""
+                    }
+                    to="/blog"
+                    onClick={handleOpenNav}
+                  >
+                    Our Blog
+                  </NavLink>
                 </li>
-                <Link to="/pricing" className='bg-primary text-white rounded-lg px-2 sm:px-5 py-1.5 sm:py-2 text-sm font-medium flex shrink-0 transition duration-200 active:scale-100 hover:scale-90'>
+                <li className="mr-0 md:mr-6 lg:mr-11 mb-5 md:mb-0 hover:underline decoration-primary underline-offset-8 decoration-2">
+                  <NavLink
+                    className={({ isActive }) =>
+                      isActive ? "underline underline-offset-8" : ""
+                    }
+                    to="/contact"
+                    onClick={handleOpenNav}
+                  >
+                    Contact us
+                  </NavLink>
+                </li>
+                <Link
+                  to="/pricing"
+                  className="bg-primary text-white rounded-lg px-2 sm:px-5 py-1.5 sm:py-2 text-sm font-medium flex shrink-0 transition duration-200 active:scale-100 hover:scale-90"
+                >
                   Our Pricing
                 </Link>
               </ul>
@@ -65,7 +120,7 @@ const Navbar = () => {
         </div>
       </header>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/DigiSkills.jsx
+++ src/pages/DigiSkills.jsx
@@ -1,19 +1,22 @@
 import '../App.css'
+import { Layout } from '../components';
 
 const DigiSkills = () => {
   
   const tallyEmbedSrc = `https://tally.so/embed/mVBxXv?alignLeft=1&hideTitle=1&transparentBackground=1&dynamicHeight=1`;
 
   return (
-    <div>
-      <iframe
-        src={tallyEmbedSrc}
-        loading="lazy"
-        width="100%"
-        className="h-screen w-full max-w-[700px] mx-auto"
-        title="Digital Skills Training for Women"
-      ></iframe>
-    </div>
+    <Layout>
+        <div>
+        <iframe
+            src={tallyEmbedSrc}
+            loading="lazy"
+            width="100%"
+            className="h-screen w-full max-w-[700px] mx-auto"
+            title="Digital Skills Training for Women"
+        ></iframe>
+        </div>
+    </Layout>
   );
 };
 
