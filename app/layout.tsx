c10e0af9c4c9fdd59d3a960399de406da92226cf
rahmlad-aramide <abdrahmanoladimeji02@gmail.com>
2026-07-04 01:40:10 +0100

    feat: add meta tag and opengraph image for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,34 @@
 import type { Metadata } from 'next';
+import Script from 'next/script';
 import './globals.css';
 import Navbar from '@/components/navbar';
 import Footer from '@/components/footer';
 import { onest } from '@/fonts';
 
 export const metadata: Metadata = {
-  title:
-    'Ridot Software Academy | Tech Training for Women & Teenagers in Data Analytics, Web Development & Digital Marketing',
+  title: 'Ridot Software Academy',
   description:
-    'Ridot Software Academy empowers women and teenagers with in-demand tech skills including data science, data analytics, web development, and digital marketing. Start your tech journey today.',
+    'Ridot Software Academy: Empowering Future Developers with Cutting-edge Training in Software Development. Explore our comprehensive programs, expert instructors, and hands-on learning to kickstart your career in the world of coding. Join us for a transformative journey in software education.',
+  openGraph: {
+    type: 'website',
+    title: 'Ridot Software Academy',
+    description:
+      'Ridot Software Academy: Empowering Future Developers with Cutting-edge Training in Software Development. Explore our comprehensive programs, expert instructors, and hands-on learning to kickstart your career in the world of coding. Join us for a transformative journey in software education.',
+    images: [
+      {
+        url: 'https://res.cloudinary.com/dh4rm7b7b/image/upload/v1699979101/Ridot/logo2d_iz0j94.jpg',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Ridot Software Academy',
+    description:
+      'Ridot Software Academy: Empowering Future Developers with Cutting-edge Training in Software Development. Explore our comprehensive programs, expert instructors, and hands-on learning to kickstart your career in the world of coding. Join us for a transformative journey in software education.',
+    images: [
+      'https://res.cloudinary.com/dh4rm7b7b/image/upload/v1699979101/Ridot/logo2d_iz0j94.jpg',
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -19,6 +39,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${onest.variable} font-onest antialiased`}>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '152480291168856');
+fbq('track', 'PageView');`}
+        </Script>
         <Navbar />
         {children}
         <Footer />
